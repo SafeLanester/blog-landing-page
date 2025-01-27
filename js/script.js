d75285635f85b401d0288f1964527e50541a5a41
WillWhyte <127570857+WillWhyte@users.noreply.github.com>
2025-01-26 19:58:00 -0500

    Changes

--- js/script.js
+++ js/script.js
@@ -5,12 +5,12 @@ document.addEventListener("DOMContentLoaded", function() {
     
     function playGlow(animationSpans, index = 0) {
         animationSpans.forEach(span => {
-        span.classList.remove('active');
-        // Re-trigger glow animation
-        span.style.animationName = 'none';
-        setTimeout(() => {
-            span.style.animationName = 'glow';
-        }, 10);
+            span.classList.remove('active');
+            // Re-trigger glow animation
+            span.style.animationName = 'none';
+            setTimeout(() => {
+                span.style.animationName = 'glow';
+            }, 10);
         });
         if (index < animationSpans.length) {
             animationSpans[index].classList.add('active');
@@ -21,7 +21,7 @@ document.addEventListener("DOMContentLoaded", function() {
         }
         setTimeout(() => playGlow(animationSpans, index), 100); // Adjust the timing as needed
     }
-    
+
     function startGlow() {
         playGlow(animation1);
     }
@@ -30,22 +30,26 @@ document.addEventListener("DOMContentLoaded", function() {
         heroImage.style.animationName = 'none';
         setTimeout(() => {
             heroImage.style.animationName = 'spin';
-        }, 10) // Quick reset to re-trigger the animation 
+        }, 10); // Quick reset to re-trigger the animation 
     }
-    
+
     function startVibrate() {
         cards.forEach(card => {
             card.classList.add('active');
             setTimeout(() => card.classList.remove('active'), 5000); // Stop vibrating after 5 seconds
         });
     }
-    
+
     function startAnimations() {
         startGlow();
         startSpin();
         startVibrate();
     }
 
-    startAnimations();
-    setInterval(startAnimations, 30000); // Repeat every 30 seconds
+    // Delay the initial animation start by 30 seconds
+    setTimeout(function() {
+        startAnimations();
+        // After the initial delay, repeat animations every 30 seconds
+        setInterval(startAnimations, 30000);
+    }, 30000); // Initial delay of 30 seconds
 });
